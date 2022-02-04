9f79b2615ebce6c14c2aa6c7690a045662f1f52a
SpiceBureau <sebesic.dominik@gmail.com>
2022-02-04 12:38:07 +0100

    "Login screen"

--- app/src/main/java/com/example/android/rest_testing/LoginActivity.kt
+++ app/src/main/java/com/example/android/rest_testing/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,11 +12,22 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         val btnEnterApp = findViewById<Button>(R.id.btnEnterApp)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        val btnRegister = findViewById<Button>(R.id.btnRegister)
+        val LoginScreen = findViewById<ConstraintLayout>(R.id.LoginScreen)
 
         btnEnterApp.setOnClickListener {
             val loadingActivity = LoadingActivity()
             val intent = Intent(this, loadingActivity::class.java)
             startActivity(intent)
+        }
+
+        btnLogin.setOnClickListener {
+            LoginScreen.removeView(it)
+        }
+
+        btnRegister.setOnClickListener {
+            LoginScreen.removeView(it)
         }
     }
 }
