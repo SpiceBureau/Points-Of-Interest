d6d716997f21e991cfbaf59350654a906fa53af0
SpiceBureau <sebesic.dominik@gmail.com>
2022-02-17 13:57:25 +0100

    "merged favorites into main"

--- app/src/main/java/com/example/android/rest_testing/net/RestFactory.kt
+++ app/src/main/java/com/example/android/rest_testing/net/RestFactory.kt
@@ -4,7 +4,7 @@ import com.example.android.rest_testing.net.retrofit.RestRetrofit
 
 object RestFactory {
 //    val BASE_IP = "10.0.2.2"
-    val BASE_IP = "192.168.0.106" //za isprobavanje na mobitelu
+    val BASE_IP = "192.168.100.32" //za isprobavanje na mobitelu
 
     val instance: RestInterface
         get() = RestRetrofit()
